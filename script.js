48997bb5a8778bfbd5d2db5c4e80181ca4921866
btarache <benjamintarache@gmail.com>
2023-06-15 09:36:26 -0500

    Search bar works

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 // SEARCH
 
 const triggerOpen = document.querySelectorAll('[trigger-button]');
-const triggerClose = document.querySelectorAll('[trigger-close]');
+const triggerClose = document.querySelectorAll('[close-button]');
 const overlay = document.querySelector('[data-overlay]'); 
 
     for (let i = 0; i < triggerOpen.length; i++) {
-        let currentId = triggerOpen[i]dataset.target, 
-        targetEl  = document.querySelector
+        let currentId = triggerOpen[i].dataset.target, 
+        targetEl = document.querySelector(`#${currentId}`)
+
+        const openData = function() {
+            targetEl.classList.remove('active');
+            overlay.classList.remove('active');
+        };
+        triggerOpen[i].addEventListener('click', function() {
+            targetEl.classList.add('active');
+            overlay.classList.add('active');    
+        });
+
+        targetEl.querySelector('[close-button]').addEventListener('click' , openData);
+        overlay.addEventListener('click' , openData);
     }
